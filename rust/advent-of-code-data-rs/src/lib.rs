73298ff06597906f237bfbd368c434f892060910
Liam Kalir <liam.kalir@protonmail.com>
2021-12-17 09:47:22 -0500

    2021 day 17 inputs

--- rust/advent-of-code-data-rs/src/lib.rs
+++ rust/advent-of-code-data-rs/src/lib.rs
@@ -410,10 +410,13 @@ pub mod aoc_2021 {
     /// 2021 Day 16: Packet Decoder
     pub const DAY16: &str = concat!(include_str!("advent-of-code-data/2021/day16.txt"), "\0");
 
+    /// 2021 Day 17: Trick Shot
+    pub const DAY17: &str = concat!(include_str!("advent-of-code-data/2021/day17.txt"), "\0");
+
     /// Array containing all inputs in chronological order
     pub const INPUTS: &[&str] = &[
         DAY1, DAY2, DAY3, DAY4, DAY5, DAY6, DAY7, DAY8, DAY9, DAY10, DAY11, DAY12, DAY13, DAY14,
-        DAY15, DAY16,
+        DAY15, DAY16, DAY17,
     ];
 }
 
