40c430f002ce641961fbc8fe97a47ce2d005ca0a
Liam Kalir <liam.kalir@protonmail.com>
2022-02-09 21:25:14 -0500

    Removed vector from cpp 2015 day 2 solution

--- cpp/src/aoc_2015/day2.cpp
+++ cpp/src/aoc_2015/day2.cpp
@@ -1,13 +1,13 @@
 #include <algorithm>
-#include <aoc.h>
 #include <cstdio>
 #include <numeric>
 #include <string>
 #include <string_view>
 #include <utility>
 
-#include "util.h"
+#include <aoc.h>
 
+#include "util.h"
 
 class Rectangle {
   public:
@@ -34,24 +34,6 @@ class Rectangle {
         return 2 * (dims.first + dims.second);
     }
 
-    static auto from_sv(const std::string_view &input) -> const std::vector<Rectangle>
-    {
-        std::vector<Rectangle> rects;
-
-        aoc::for_each_line(input, [&](auto line) {
-            size_t subpos = 0, subpos2 = 0;
-            subpos2 = line.find('x', subpos);
-            auto length = aoc::svatoi(line.substr(0, subpos2));
-            subpos = line.find('x', subpos2 + 1);
-            auto width = aoc::svatoi(line.substr(subpos2 + 1, subpos - subpos2 - 1));
-            subpos2 = subpos + 1;
-            auto height = aoc::svatoi(line.substr(subpos2, line.length() - subpos2));
-            rects.emplace_back(length, width, height);
-        });
-
-        return rects;
-    }
-
   private:
     [[nodiscard]] auto get_smallest_sides() const -> std::pair<int, int>
     {
@@ -59,6 +41,7 @@ class Rectangle {
         std::sort(dims.begin(), dims.end());
         return std::make_pair(dims[0], dims[1]);
     }
+
     const int m_length;
     const int m_width;
     const int m_height;
@@ -71,26 +54,44 @@ template <> auto AocSolution<2, 2015>::name() const -> const std::string &
     return day2_name;
 }
 
-static auto part1(int x, const Rectangle &rect) -> int
-{
-    return x + rect.surface_area() + rect.slack();
-}
-
-static auto part2(int x, const Rectangle &rect) -> int
-{
-    return x + rect.volume() + rect.bow();
-}
-
 template <> auto AocSolution<2, 2015>::part1(const std::string_view &input) const -> int
 {
-    auto rects = Rectangle::from_sv(input);
     auto sum = 0;
-    return std::accumulate(rects.begin(), rects.end(), sum, ::part1);
+
+    aoc::for_each_line(input, [&sum](auto &line) {
+        size_t subpos = 0, subpos2 = 0;
+        subpos2 = line.find('x', subpos);
+        auto length = aoc::svatoi(line.substr(0, subpos2));
+        subpos = line.find('x', subpos2 + 1);
+        auto width = aoc::svatoi(line.substr(subpos2 + 1, subpos - subpos2 - 1));
+        subpos2 = subpos + 1;
+        auto height = aoc::svatoi(line.substr(subpos2, line.length() - subpos2));
+
+        Rectangle rect(length, width, height);
+
+        sum += rect.surface_area() + rect.slack();
+    });
+
+    return sum;
 }
 
 template <> auto AocSolution<2, 2015>::part2(const std::string_view &input) const -> int
 {
-    auto rects = Rectangle::from_sv(input);
     auto sum = 0;
-    return std::accumulate(rects.begin(), rects.end(), sum, ::part2);
+
+    aoc::for_each_line(input, [&sum](auto &line) {
+        size_t subpos = 0, subpos2 = 0;
+        subpos2 = line.find('x', subpos);
+        auto length = aoc::svatoi(line.substr(0, subpos2));
+        subpos = line.find('x', subpos2 + 1);
+        auto width = aoc::svatoi(line.substr(subpos2 + 1, subpos - subpos2 - 1));
+        subpos2 = subpos + 1;
+        auto height = aoc::svatoi(line.substr(subpos2, line.length() - subpos2));
+
+        Rectangle rect(length, width, height);
+
+        sum += rect.volume() + rect.bow();
+    });
+
+    return sum;
 }
